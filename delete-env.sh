e95e493e226ff50c49f01ae6bb4c6c7e170828ff
Peter Marciano <petemarciano2@gmail.com>
2016-10-07 18:22:12 +0000

    Added Week 5 assignment.

---
+++ delete-env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#grab associated instance ids
+instanceNameArray=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name week4asg | cut -f4 | grep -E 'i-'`
+
+#aws autoscaling detach load-balancers from autoscaling group
+aws autoscaling detach-load-balancers --load-balancer-names pmarcian-lb --auto-scaling-group-name week4asg
+
+#aws autoscaling delete autoscaling configuration
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name week4asg --force-delete
+
+#wait for instances to terminate
+aws ec2 wait instance-terminated --instance-ids $instanceNameArray
+
+
+#aws autoscaling delete-launch configuration
+aws autoscaling delete-launch-configuration --launch-configuration-name week4lc
+
+#aws elb de register instances from load-balancer
+aws elb deregister-instances-from-load-balancer --load-balancer-name pmarcian-lb --instances $instanceNameArray
+
+#aws elb delete listeners 
+aws elb delete-load-balancer-listeners --load-balancer-name pmarcian-lb --load-balancer-ports 80
+
+#aws elb delete load-balancers
+aws elb delete-load-balancer --load-balancer-name pmarcian-lb
+
+#aws ec2 terminate-instances
+#aws ec2 terminate-instances --instance-ids $instanceNameArray
+
+exit 0
