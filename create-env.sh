31691acce89f5af2614ae88b86eef6425b4618ec
Peter Marciano <petemarciano2@gmail.com>
2016-10-16 00:55:55 +0000

    Added positional parameter variables

--- create-env.sh
+++ create-env.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-<<<<<<< HEAD
-
 # now with positional parameters
-# AMI ID, key-name, security-group, launch-configuration, count
-# $1    , $2      , $3            , $4                  , $5
-=======
+# AMI ID      , key-name, security-group, launch-configuration, count
+# ami-06b94666, week3   , sg-a0df10d9   , week4lc             , #
+# $1          , $2      , $3            , $4                  , $5
 # Peter Marciano II
 # Week 4 Assignment
->>>>>>> 2469d96df7035641149435dc48bcae0da69a6062
 
-aws ec2 run-instances --image-id ami-06b94666 --count 3 --key-name week3 --security-group-ids sg-a0df10d9 --instance-type t2.micro --user-data file://installapp.sh --placement AvailabilityZone=us-west-2a
+aws ec2 run-instances --image-id $1 --count $5 --key-name $2 --security-group-ids $3 --instance-type t2.micro --user-data file://installapp.sh --placement AvailabilityZone=us-west-2a
 
 aws elb create-load-balancer --load-balancer-name pmarcian-lb --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones us-west-2a
 
@@ -19,8 +16,8 @@ instanceNameArray=`aws ec2 describe-instances --filters Name=instance-state-name
 
 aws elb register-instances-with-load-balancer --load-balancer-name pmarcian-lb --instances $instanceNameArray
 
-aws autoscaling create-launch-configuration --launch-configuration-name week4lc --image-id ami-06b94666 --key-name week3 --instance-type t2.micro --user-data file://installapp.sh
+aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --key-name $2 --instance-type t2.micro --user-data file://installapp.sh
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name week4asg --launch-configuration week4lc --availability-zone us-west-2a --load-balancer-name pmarcian-lb --max-size 5 --min-size 1 --desired-capacity 3
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name week4asg --launch-configuration $4 --availability-zone us-west-2a --load-balancer-name pmarcian-lb --max-size 5 --min-size 1 --desired-capacity 3
 
 exit 0
