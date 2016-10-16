7cea23e60229698ee6cb75520c32d75db1bebe43
Peter Marciano <petemarciano2@gmail.com>
2016-10-16 01:07:50 +0000

    Added logic to quit of not 5 parameters are entered

--- create-env.sh
+++ create-env.sh
@@ -6,18 +6,25 @@
 # Peter Marciano II
 # Week 4 Assignment
 
-aws ec2 run-instances --image-id $1 --count $5 --key-name $2 --security-group-ids $3 --instance-type t2.micro --user-data file://installapp.sh --placement AvailabilityZone=us-west-2a
+if [ $# -eq 5 ]; then
 
-aws elb create-load-balancer --load-balancer-name pmarcian-lb --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones us-west-2a
+  aws ec2 run-instances --image-id $1 --count $5 --key-name $2 --security-group-ids $3 --instance-type t2.micro --user-data file://installapp.sh --placement AvailabilityZone=us-west-2a
 
-sleep 30
+  aws elb create-load-balancer --load-balancer-name pmarcian-lb --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones us-west-2a
 
-instanceNameArray=`aws ec2 describe-instances --filters Name=instance-state-name,Values=running --output text | cut -f8 | grep -E 'i-'`
+  sleep 30
 
-aws elb register-instances-with-load-balancer --load-balancer-name pmarcian-lb --instances $instanceNameArray
+  instanceNameArray=`aws ec2 describe-instances --filters Name=instance-state-name,Values=running --output text | cut -f8 | grep -E 'i-'`
 
-aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --key-name $2 --instance-type t2.micro --user-data file://installapp.sh
+  aws elb register-instances-with-load-balancer --load-balancer-name pmarcian-lb --instances $instanceNameArray
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name week4asg --launch-configuration $4 --availability-zone us-west-2a --load-balancer-name pmarcian-lb --max-size 5 --min-size 1 --desired-capacity 3
+  aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --key-name $2 --instance-type t2.micro --user-data file://installapp.sh
+
+  aws autoscaling create-auto-scaling-group --auto-scaling-group-name week4asg --launch-configuration $4 --availability-zone us-west-2a --load-balancer-name pmarcian-lb --max-size 5 --min-size 1 --desired-capacity 3
+else
+  echo "You need 5 positional parameters!"
+  echo "ami id, key-name, security-group, launch-config, count"
+  exit 1
+fi
 
 exit 0
